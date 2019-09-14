b30d0e7fd45938ee630aa2c242c4ba680abc3b95
Murat Özkan <smuratozkan@gmail.com>
2019-09-14 14:59:39 +0200

    4.1 Parity

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -2,10 +2,25 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class Parity {
+  /* parity is 1 if number of 1's is odd, 0 otherwise */
   @EpiTest(testDataFile = "parity.tsv")
-  public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+  public static short parityBruteForce(long x) {
+    short parity = 0;
+    while (x != 0) {
+      parity ^= (x & 1);
+      x >>>= 1;
+    }
+    return parity;
+  }
+
+  @EpiTest(testDataFile = "parity.tsv")
+  public static short parityBetter(long x) {
+    short parity = 0;
+    while (x != 0) {
+      parity ^= 1;
+      x &= (x - 1);
+    }
+    return parity;
   }
 
   public static void main(String[] args) {
