2c4dff1f6c24e2868bf644b6e68ed1dc2daf3adf
Murat Özkan <smuratozkan@gmail.com>
2019-10-06 20:14:17 +0200

    7.2 Reverse Sublist

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -6,8 +6,33 @@ public class ReverseSublist {
 
   public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
                                                  int finish) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> h = new ListNode<>(-100, L);
+    ListNode<Integer> rH = new ListNode<>(-200, null);
+    ListNode<Integer> prev = h, curr = h.next;  // iterating main list
+    ListNode<Integer> sH, sL = null;   // sublist head and last element
+
+    int i = 1;
+    for (;i < start; i++) {
+      prev = curr;
+      curr = curr.next;
+    }
+    sH = prev;
+    for (; i <= finish; i++) {
+      prev = curr;
+      curr = curr.next;
+      if (rH.next == null) {
+        sL = prev;
+      }
+      ListNode<Integer> t = rH.next;
+      rH.next = prev;
+      prev.next = t;
+    }
+    if (sH != null && sL != null) {
+      sH.next = rH.next;
+      sL.next = curr;
+    }
+
+    return h.next;
   }
 
   public static void main(String[] args) {
