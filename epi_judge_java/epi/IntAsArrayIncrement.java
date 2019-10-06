42aa15126eff49e3b2b76b54b7322a2635106c4b
Murat Özkan <smuratozkan@gmail.com>
2019-10-06 21:33:26 +0200

    5.2 Increment Int as Array

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -6,8 +6,17 @@ import java.util.List;
 public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    int carry = 1;
+    for (int i = A.size() - 1; i >= 0; i--) {
+      int d = A.get(i) + carry;
+      A.set(i, d % 10);
+      carry = d / 10;
+    }
+    if (carry == 1) {
+      A.add(0);       // only way to have a 1 carry over is to have a 0 all the way, so we're adding a 0 to the LSD
+      A.set(0, 1);
+    }
+    return A;
   }
 
   public static void main(String[] args) {
