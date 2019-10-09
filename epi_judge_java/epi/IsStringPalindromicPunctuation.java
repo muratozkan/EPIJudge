826244be151401e88ab6fb94535d67e7892905e4
Murat Özkan <smuratozkan@gmail.com>
2019-10-09 18:33:32 +0200

    6.4 Test Palindromicity

--- epi_judge_java/epi/IsStringPalindromicPunctuation.java
+++ epi_judge_java/epi/IsStringPalindromicPunctuation.java
@@ -2,10 +2,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsStringPalindromicPunctuation {
-  @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
 
+  @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
   public static boolean isPalindrome(String s) {
-    // TODO - you fill in here.
+    int b = 0;
+    int e = s.length() - 1;
+    while (e > b) {
+      char ec = s.charAt(e);
+      if (!Character.isLetterOrDigit(ec)) {
+        e --;
+        continue;
+      }
+      char bc = s.charAt(b);
+      if (!Character.isLetterOrDigit(bc)) {
+        b ++;
+        continue;
+      }
+      if (Character.toLowerCase(bc) != Character.toLowerCase(ec)) {
+        return false;
+      }
+      e --;
+      b ++;
+    }
+
     return true;
   }
 
