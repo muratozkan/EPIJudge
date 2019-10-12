26dbf25a7d30c7340b325247af93376dfa9fe59c
Murat Özkan <smuratozkan@gmail.com>
2019-10-12 19:51:02 +0200

    9.1 Is Tree Balanced

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -3,11 +3,39 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+  static class BalanceHeight {
+    boolean balanced;
+    int height;
 
+    BalanceHeight(boolean balanced, int height) {
+      this.balanced = balanced;
+      this.height = height;
+    }
+  }
+
+  private static BalanceHeight checkHeight(BinaryTreeNode<Integer> root) {
+    BalanceHeight current = new BalanceHeight(true, -1);
+    if (root == null) {
+      return current;
+    }
+    BalanceHeight left, right;
+    left = checkHeight(root.left);
+    if (!left.balanced) {
+      return left;
+    }
+    right = checkHeight(root.right);
+    if (!right.balanced) {
+      return right;
+    }
+
+    current.balanced = Math.abs(left.height - right.height) <= 1;
+    current.height = Math.max(right.height, left.height) + 1;
+    return current;
+  }
+
+  @EpiTest(testDataFile = "is_tree_balanced.tsv")
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return checkHeight(tree).balanced;
   }
 
   public static void main(String[] args) {
