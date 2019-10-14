692c411f2449699f15392d2a04e5eceb8c8f7f3a
Murat Özkan <smuratozkan@gmail.com>
2019-10-14 21:02:24 +0200

    7.7 Remove kth Last Element from List

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -3,11 +3,25 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class DeleteKthLastFromList {
   @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
-
   // Assumes L has at least k nodes, deletes the k-th last node in L.
   public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> lastI = new ListNode<>(Integer.MIN_VALUE, L);
+    ListNode<Integer> remI = new ListNode<>(Integer.MIN_VALUE, L);
+
+    for (int i = 0; i < k; i++) {
+      lastI = lastI.next;
+    }
+
+    while (lastI.next != null) {
+      lastI = lastI.next;
+      remI = remI.next;
+    }
+    // remI.next is the node to delete now
+    ListNode<Integer> toDelete = remI.next;
+    remI.next = toDelete == null ? null : toDelete.next;
+
+    // if deleting the head node, the list is the remaining
+    return toDelete == L ? remI.next : L;
   }
 
   public static void main(String[] args) {
