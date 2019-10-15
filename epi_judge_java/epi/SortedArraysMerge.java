b3944a38e08e7a959ad53e0738ed7f0e75c1f365
Murat Özkan <smuratozkan@gmail.com>
2019-10-15 21:14:43 +0200

    10.1 Merge Sorted Arrays

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -2,14 +2,42 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
-public class SortedArraysMerge {
-  @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+import java.util.PriorityQueue;
 
-  public static List<Integer>
-  mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
+public class SortedArraysMerge {
+
+  private static class MinRef {
+    Integer value;
+    int aI;
+    int i;
+
+    MinRef(Integer value, int aI, int i) {
+      this.value = value;
+      this.aI = aI;
+      this.i = i;
+    }
+  }
+
+  @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
+  public static List<Integer> mergeSortedArrays(List<List<Integer>> sortedArrays) {
+    PriorityQueue<MinRef> minHeap = new PriorityQueue<>(sortedArrays.size(), Comparator.comparingInt(r -> r.value));
+    List<Integer> sorted = new LinkedList<>();
+    for (int i = 0; i < sortedArrays.size(); i++) {
+      List<Integer> a = sortedArrays.get(i);
+      if (!a.isEmpty())
+        minHeap.add(new MinRef(a.get(0), i, 0));
+    }
+    while (!minHeap.isEmpty()) {
+      MinRef min = minHeap.remove();
+      if (sortedArrays.get(min.aI).size() > (min.i + 1)) {
+        minHeap.add(new MinRef(sortedArrays.get(min.aI).get(min.i + 1), min.aI, min.i + 1));
+      }
+      sorted.add(min.value);
+    }
+    return sorted;
   }
 
   public static void main(String[] args) {
