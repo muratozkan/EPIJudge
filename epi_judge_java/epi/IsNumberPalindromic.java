8dc288bde9fe0c69139f0f9dc40299dc2fe415a6
Murat Özkan <smuratozkan@gmail.com>
2019-10-16 21:51:22 +0200

    4.9 Is Number Palindromic

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -4,7 +4,22 @@ import epi.test_framework.GenericTest;
 public class IsNumberPalindromic {
   @EpiTest(testDataFile = "is_number_palindromic.tsv")
   public static boolean isPalindromeNumber(int x) {
-    // TODO - you fill in here.
+    if (x <= 0) {
+      return x == 0;
+    }
+    // we don't want to check the string version to avoid O(n) space complexity
+    int n = (int) Math.log10(x);
+    int p = (int) Math.pow(10, n);
+    int d = x;
+    for (; n > 0; n -= 2) {
+      int msd = d / p;
+      int lsd = d % 10;
+      if (msd != lsd) {
+        return false;
+      }
+      d = (d % p) / 10;
+      p /= 100;
+    }
     return true;
   }
 
