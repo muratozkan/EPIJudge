227c6cb11c2fe33104c75ec28e368148489238e1
Murat Özkan <smuratozkan@gmail.com>
2019-10-18 23:28:24 +0200

    8.2 Evaluate RPN

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,12 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class EvaluateRpn {
   @EpiTest(testDataFile = "evaluate_rpn.tsv")
-
   public static int eval(String expression) {
-    // TODO - you fill in here.
-    return 0;
+    Map<String, BiFunction<Integer, Integer, Integer>> OPS = Map.of(
+            "+", Integer::sum,
+            "-", (b, a) -> a - b,
+            "*", (b, a) -> a * b,
+            "/", (b, a) -> a / b);
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (String s : expression.split(",")) {
+      if (s.length() == 1 && OPS.containsKey(s)) {
+        stack.push(OPS.get(s).apply(stack.pop(),  stack.pop()));
+      } else {
+        stack.push(Integer.valueOf(s));
+      }
+    }
+    return stack.pop();
   }
 
   public static void main(String[] args) {
