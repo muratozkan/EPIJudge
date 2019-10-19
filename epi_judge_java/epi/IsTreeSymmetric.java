1ddda0d4ca8e3fbc4e49555e48339d3cf308dad0
Murat Özkan <smuratozkan@gmail.com>
2019-10-19 13:19:10 +0200

    9.2 Is Tree Symmetric

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,12 +1,24 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class IsTreeSymmetric {
-  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
+import java.util.Objects;
+
+public class IsTreeSymmetric {
+
+  private static boolean symmetricWalk(BinaryTreeNode<Integer> left, BinaryTreeNode<Integer> right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left != null && right != null) {
+      return Objects.equals(left.data, right.data) && symmetricWalk(left.left, right.right) && symmetricWalk(left.right, right.left);
+    }
+    return false;
+  }
+
+  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return tree == null || symmetricWalk(tree.left, tree.right);
   }
 
   public static void main(String[] args) {
