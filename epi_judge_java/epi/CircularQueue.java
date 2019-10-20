a91e64514b82254d5fd9836d1bcb558a4f69bd90
Murat Özkan <smuratozkan@gmail.com>
2019-10-20 15:16:19 +0200

    8.7 Circular Queue using Array

--- epi_judge_java/epi/CircularQueue.java
+++ epi_judge_java/epi/CircularQueue.java
@@ -4,27 +4,55 @@ import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
+import java.util.Arrays;
 import java.util.List;
 public class CircularQueue {
 
   public static class Queue {
-    public Queue(int capacity) {}
+    private Integer[] array;
+    private int start = 0, end = 0, size = 0;
+    private int capacity;
+    public Queue(int capacity) {
+      this.capacity = capacity;
+      array = new Integer[this.capacity];
+    }
+
+    private void grow() {
+      array = Arrays.copyOf(array, capacity * 2);
+      if (start == end) {
+        for (int i = 0; i < end; i++) {
+          array[capacity + i] = array[i];
+          array[i] = null;
+        }
+        end = capacity + end;
+      }
+      capacity *= 2;
+    }
+
     public void enqueue(Integer x) {
-      // TODO - you fill in here.
-      return;
+      if (size + 1 > array.length) {
+        grow();
+      }
+
+      array[end] = x;
+      size ++;
+      end = (end + 1) % array.length;
     }
+
     public Integer dequeue() {
-      // TODO - you fill in here.
-      return 0;
+      Integer i = array[start];
+      array[start] = null;
+      size --;
+      start = (start + 1) % array.length;
+      return i;
     }
+
     public int size() {
-      // TODO - you fill in here.
-      return 0;
+      return size;
     }
     @Override
     public String toString() {
-      // TODO - you fill in here.
-      return super.toString();
+      return Arrays.toString(array);
     }
   }
   @EpiUserType(ctorParams = {String.class, int.class})
