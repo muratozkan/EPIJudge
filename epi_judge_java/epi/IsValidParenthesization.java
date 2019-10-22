3c1191d686f93348052181768ce1f4207801f7d6
Murat Özkan <smuratozkan@gmail.com>
2019-10-22 20:43:54 +0200

    8.3 Is Valid Paranthesization

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,12 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
+    Deque<Character> stack = new ArrayDeque<>(s.length());
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == '(' || c == '[' || c == '{') {
+        stack.push(c);
+      } else if (stack.isEmpty()) {
+          return false;
+      } else {
+        Character oC = stack.removeFirst();
+        if (c == ')' && oC != '(') {
+          return false;
+        }
+        if (c == ']' && oC != '[') {
+          return false;
+        }
+        if (c == '}' && oC != '{') {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {
