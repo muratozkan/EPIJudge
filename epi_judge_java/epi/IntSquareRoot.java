0d694078f7f22dcbbe5c92a30d682536b5264004
Murat Özkan <smuratozkan@gmail.com>
2019-10-22 20:25:07 +0200

    11.4 Int Square Root

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -5,8 +5,16 @@ public class IntSquareRoot {
   @EpiTest(testDataFile = "int_square_root.tsv")
 
   public static int squareRoot(int k) {
-    // TODO - you fill in here.
-    return 0;
+    int low = 0, up = Math.min(k, 46340);
+    while (low <= up) {
+      int mid = low + (up - low) / 2;
+      if (mid * mid <= k) {
+        low = mid + 1;
+      } else if (mid * mid > k) {
+        up = mid - 1;
+      }
+    }
+    return low - 1;
   }
 
   public static void main(String[] args) {
