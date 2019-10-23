418da2f67fd605d80c4e317740856d95660c7e7a
Murat Özkan <smuratozkan@gmail.com>
2019-10-23 19:49:09 +0200

    12.2 Is Letter Constructable from Magazine

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,12 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
   public static boolean isLetterConstructibleFromMagazine(String letterText,
                                                           String magazineText) {
-    // TODO - you fill in here.
+    Map<Character, Integer> mc = new HashMap<>();
+    for (int i = 0; i < magazineText.length(); i++) {
+      int c = mc.getOrDefault(magazineText.charAt(i), 0);
+      mc.put(magazineText.charAt(i), c + 1);
+    }
+
+    for (int i = 0; i < letterText.length(); i++) {
+      int c = mc.getOrDefault(letterText.charAt(i), 0);
+      if (c == 0) {
+        return false;
+      }
+      mc.put(letterText.charAt(i), c - 1);
+    }
+
     return true;
   }
 
