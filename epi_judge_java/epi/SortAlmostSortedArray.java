48342dc8f149a47151ddbbc422369747c2190064
Murat Özkan <smuratozkan@gmail.com>
2019-10-24 20:11:24 +0200

    10.2 Sort Almost Sorted Array

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -2,14 +2,31 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
+
 public class SortAlmostSortedArray {
 
   public static List<Integer>
   sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
-    // TODO - you fill in here.
-    return null;
+    int size = k + 1;
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(size, Comparator.naturalOrder());
+    List<Integer> list = new ArrayList<>();
+    while (sequence.hasNext()) {
+      if (minHeap.size() < size) {
+        minHeap.add(sequence.next());
+      }
+      if (minHeap.size() == size) {
+        list.add(minHeap.poll());
+      }
+    }
+    while (!minHeap.isEmpty()) {
+      list.add(minHeap.poll());
+    }
+    return list;
   }
   @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
   public static List<Integer>
