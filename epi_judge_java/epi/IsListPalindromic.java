988b82ca2278ba081b6dac91d1ebac94dc335602
Murat Özkan <smuratozkan@gmail.com>
2019-10-25 22:04:41 +0200

    7.11 Is List Palindromic

--- epi_judge_java/epi/IsListPalindromic.java
+++ epi_judge_java/epi/IsListPalindromic.java
@@ -2,10 +2,47 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsListPalindromic {
+
+  private static ListNode<Integer> reverse(ListNode<Integer> head) {
+    ListNode<Integer> next, prev = null, current = head;
+    while (current != null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    return prev;
+  }
+
   @EpiTest(testDataFile = "is_list_palindromic.tsv")
 
   public static boolean isLinkedListAPalindrome(ListNode<Integer> L) {
-    // TODO - you fill in here.
+    int k = -1;
+    ListNode<Integer> it, head = new ListNode<>(Integer.MIN_VALUE, L);
+    it = head;
+    while (it.next != null) {
+      it = it.next;
+      k ++;
+    }
+    if (k == 0 || k == 1) {
+      return true;
+    }
+
+    it = head;
+    for (int i = 0; i < k / 2 ; i++) {
+      it = it.next;
+    }
+    ListNode<Integer> rHead = reverse(it), rit;
+    it = head.next;
+    rit = rHead;
+    while (it != null && rit != null) {
+      if (!it.data.equals(rit.data)) {
+        return false;
+      }
+      it = it.next;
+      rit = rit.next;
+    }
+
     return true;
   }
 
