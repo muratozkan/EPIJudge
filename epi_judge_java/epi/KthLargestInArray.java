0ca5b27345483d12cd7296a83f816d5c9c2ae787
Murat Özkan <smuratozkan@gmail.com>
2019-10-28 21:23:11 +0100

    11.8 Kth Largest in Array

--- epi_judge_java/epi/KthLargestInArray.java
+++ epi_judge_java/epi/KthLargestInArray.java
@@ -2,15 +2,26 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+
 public class KthLargestInArray {
   // The numbering starts from one, i.e., if A = [3,1,-1,2] then
   // findKthLargest(1, A) returns 3, findKthLargest(2, A) returns 2,
   // findKthLargest(3, A) returns 1, and findKthLargest(4, A) returns -1.
   @EpiTest(testDataFile = "kth_largest_in_array.tsv")
   public static int findKthLargest(int k, List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, Comparator.naturalOrder());
+    // O(nlog(k)) since this method sorts the heap. Space complexity: O(k)
+    for (Integer val : A) {
+      int min = Integer.MIN_VALUE;
+      if (minHeap.size() == k) {
+        min = minHeap.remove();
+      }
+      minHeap.add(Math.max(min, val));
+    }
+    return minHeap.remove();
   }
 
   public static void main(String[] args) {
