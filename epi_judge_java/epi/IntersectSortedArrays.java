f1c8a0de0eba46b7990cdeaca4788ff66028e3de
Murat Özkan <smuratozkan@gmail.com>
2019-10-29 21:30:41 +0100

    13.1 Intersect sorted arrays

--- epi_judge_java/epi/IntersectSortedArrays.java
+++ epi_judge_java/epi/IntersectSortedArrays.java
@@ -2,14 +2,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class IntersectSortedArrays {
   @EpiTest(testDataFile = "intersect_sorted_arrays.tsv")
 
   public static List<Integer> intersectTwoSortedArrays(List<Integer> A,
                                                        List<Integer> B) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> intersect = new ArrayList<>();
+    int i = 0, j = 0;
+    while (i < A.size() && j < B.size()) {
+      if (A.get(i) < B.get(j)) {
+        i ++;
+      } else if (A.get(i) > B.get(j)) {
+        j ++;
+      } else {
+        if (intersect.isEmpty() || !A.get(i).equals(intersect.get(intersect.size() - 1))) {
+          intersect.add(A.get(i));
+        }
+        i++;
+        j++;
+      }
+    }
+    return intersect;
   }
 
   public static void main(String[] args) {
