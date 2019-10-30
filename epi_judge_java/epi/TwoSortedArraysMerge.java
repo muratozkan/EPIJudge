ff41bded84ebcd8cf96525d8d11a669f340e494a
Murat Özkan <smuratozkan@gmail.com>
2019-10-30 20:44:18 +0100

    13.2 Merge Two Sorted Arrays

--- epi_judge_java/epi/TwoSortedArraysMerge.java
+++ epi_judge_java/epi/TwoSortedArraysMerge.java
@@ -2,13 +2,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class TwoSortedArraysMerge {
 
   public static void mergeTwoSortedArrays(List<Integer> A, int m,
                                           List<Integer> B, int n) {
-    // TODO - you fill in here.
-    return;
+    int al = A.size() - m;
+    Collections.rotate(A, al);
+    int wi = 0, i = 0, j = 0;
+    while (wi < m + n) {
+      int ai = (i < m) ? A.get(al + i) : Integer.MAX_VALUE;
+      int bj = (j < n) ? B.get(j) : Integer.MAX_VALUE;
+      if (ai <= bj) {
+        Collections.swap(A, wi, al + i);
+        i ++;
+      } else {
+        A.set(wi, bj);
+        j++;
+      }
+      wi++;
+    }
   }
   @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
   public static List<Integer>
