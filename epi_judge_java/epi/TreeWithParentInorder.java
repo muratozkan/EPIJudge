50ac08e2b2b57b56059f22518e5a8e1bd9279c38
Murat Özkan <smuratozkan@gmail.com>
2019-10-31 19:19:08 +0100

    9.10 Inorder Traversal with Constant Space

--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -2,13 +2,37 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class TreeWithParentInorder {
   @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> list = new ArrayList<>();
+    BinaryTree<Integer> prev = null, curr = tree;
+    while (curr != null) {
+      if (curr.right != null && curr.right == prev) {
+        prev = curr;
+        curr = curr.parent;
+      } else if (curr.left != null && curr.left != prev) {
+        prev = curr;
+        curr = curr.left;
+      } else {
+        list.add(curr.data);
+        if (curr.right != null && curr.right != prev) {
+          prev = curr;
+          curr = curr.right;
+        } else {
+          if (curr.parent == null) {
+            curr = null;
+          } else{
+            prev = (curr.parent.left == curr) ? curr : curr.parent;
+            curr = prev.parent;
+          }
+        }
+      }
+    }
+    return list;
   }
 
   public static void main(String[] args) {
