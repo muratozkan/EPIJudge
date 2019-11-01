5adbe6061e9cbe55460073d260a86a04f824dad7
Murat Özkan <smuratozkan@gmail.com>
2019-11-01 14:58:24 +0100

    12.1 Test for Palindromic Permutations

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,12 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      map.put(s.charAt(i),  map.getOrDefault(s.charAt(i), 0) + 1);
+    }
+    int evens = (int) map.values().stream().filter(i -> i % 2 == 0).count();
+    int odds = map.size() - evens;
+
+    if (s.length() % 2 == 0) {
+      return odds == 0;
+    } else {
+      return odds == 1;
+    }
   }
 
   public static void main(String[] args) {
