e56283fb90d46bf67464774ca461662b8d95a802
Murat Özkan <smuratozkan@gmail.com>
2019-11-01 14:32:44 +0100

    9.11 Binary Tree from InOrder PreOrder

--- epi_judge_java/epi/TreeFromPreorderInorder.java
+++ epi_judge_java/epi/TreeFromPreorderInorder.java
@@ -4,12 +4,23 @@ import epi.test_framework.GenericTest;
 
 import java.util.List;
 public class TreeFromPreorderInorder {
-  @EpiTest(testDataFile = "tree_from_preorder_inorder.tsv")
 
+  private static BinaryTreeNode<Integer> recursiveConstruct(List<Integer> pre, List<Integer> in, int pi, int pH, int inL, int inH) {
+    BinaryTreeNode<Integer> root = null;
+    if (pi < pH && inL < inH) {
+      int d = pre.get(pi);
+      int ri = in.subList(inL, inH).indexOf(d);
+      BinaryTreeNode<Integer> left = recursiveConstruct(pre, in, pi + 1, pi + ri + 1, inL, inL + ri);
+      BinaryTreeNode<Integer> right = recursiveConstruct(pre, in, pi + ri + 1, pH, inL + ri + 1, inH);
+      root = new BinaryTreeNode<>(d, left, right);
+    }
+    return root;
+  }
+
+  @EpiTest(testDataFile = "tree_from_preorder_inorder.tsv")
   public static BinaryTreeNode<Integer>
   binaryTreeFromPreorderInorder(List<Integer> preorder, List<Integer> inorder) {
-    // TODO - you fill in here.
-    return null;
+    return recursiveConstruct(preorder, inorder, 0, preorder.size(), 0, inorder.size());
   }
 
   public static void main(String[] args) {
