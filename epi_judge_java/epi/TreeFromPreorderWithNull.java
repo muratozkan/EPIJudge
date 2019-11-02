0add5d2d269f366ae55633df5109f589e7cf0053
Murat Özkan <smuratozkan@gmail.com>
2019-11-02 13:11:22 +0100

    9.12 Tree from PreOrder with Markers

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -6,10 +6,37 @@ import epi.test_framework.TimedExecutor;
 import java.util.ArrayList;
 import java.util.List;
 public class TreeFromPreorderWithNull {
-  public static BinaryTreeNode<Integer>
-  reconstructPreorder(List<Integer> preorder) {
-    // TODO - you fill in here.
-    return null;
+
+  static class NodeWithIndex {
+    BinaryTreeNode<Integer> node;
+    int index;
+
+    NodeWithIndex(BinaryTreeNode<Integer> node, int index) {
+      this.node = node;
+      this.index = index;
+    }
+  }
+
+  private static NodeWithIndex recursiveConstruct(List<Integer> pre, int pi) {
+    NodeWithIndex node = new NodeWithIndex(null, 1);
+    if (pre.get(pi) != null) {
+      int d = pre.get(pi);
+      node.node = new BinaryTreeNode<>(d);
+      if (pre.get(pi + 1) == null && pre.get(pi + 2) == null) {
+        node.index = 3;
+        return node;
+      }
+      NodeWithIndex left = recursiveConstruct(pre, pi + 1);
+      NodeWithIndex right = recursiveConstruct(pre, pi + left.index + 1);
+      node.node.left = left.node;
+      node.node.right = right.node;
+      node.index = left.index + right.index + 1;
+    }
+    return node;
+  }
+
+  public static BinaryTreeNode<Integer> reconstructPreorder(List<Integer> preorder) {
+    return recursiveConstruct(preorder, 0).node;
   }
   @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
   public static BinaryTreeNode<Integer>
