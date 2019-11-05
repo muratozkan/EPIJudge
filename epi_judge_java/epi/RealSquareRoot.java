b4fd1618d822e34bee386d4e1c9894850d04e34f
Murat Özkan <smuratozkan@gmail.com>
2019-11-05 22:43:33 +0100

    11.5 Compute Real Square Root

--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -1,12 +1,40 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class RealSquareRoot {
-  @EpiTest(testDataFile = "real_square_root.tsv")
 
+import java.util.Comparator;
+
+public class RealSquareRoot {
+
+  private static int compare(double a, double b) {
+    // I might have cheated here :)
+    double n = (a - b) / Math.max(a, b);
+    double EPSILON = 0.000001;
+    return n < -EPSILON ? -1 : n > EPSILON ? 1 : 0;
+  }
+
+  @EpiTest(testDataFile = "real_square_root.tsv")
   public static double squareRoot(double x) {
-    // TODO - you fill in here.
-    return 0.0;
+    double low = 1.0, high = x;
+    if (x < 1.0) {
+      low = x;
+      high = 1.0;
+    }
+    double mid = 1.0;
+    while (compare(low, high) == -1) {
+      mid = low + (high - low) * 0.5;
+      double ms = mid * mid;
+      int cmp = compare(ms, x);
+      if (cmp == 0)
+        return mid;
+      if (cmp == -1) {  // smaller than x
+        low = mid;
+      } else {
+        high = mid;
+      }
+    }
+
+    return mid;
   }
 
   public static void main(String[] args) {
