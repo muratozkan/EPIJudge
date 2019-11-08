4381455e7e62b76e058c757ea1faa04438f866cf
Murat Özkan <smuratozkan@gmail.com>
2019-11-08 20:55:50 +0100

    11.9 Find Missing Value from Large Array

--- epi_judge_java/epi/AbsentValueArray.java
+++ epi_judge_java/epi/AbsentValueArray.java
@@ -3,12 +3,37 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
+import java.util.BitSet;
 import java.util.List;
 public class AbsentValueArray {
 
+  private static final int NUM_BUCKETS = 1 << 16;
+  private static final int BUCKET_CAPACITY = 1 << 16;
+
   public static int findMissingElement(Iterable<Integer> stream) {
-    // TODO - you fill in here.
-    return 0;
+    int[] buckets = new int[NUM_BUCKETS];
+    stream.iterator().forEachRemaining(i -> buckets[i >>> 16]++);
+
+    int bucket = -1;
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+      if (buckets[i] < BUCKET_CAPACITY) {
+        bucket = i;
+        break;
+      }
+    }
+    BitSet bitSet = new BitSet(BUCKET_CAPACITY);
+    for (int i : stream) {
+      if (i >>> 16 == bucket) {
+        bitSet.set(i & 0xFFFF);
+      }
+    }
+
+    for (int i = 0; i < BUCKET_CAPACITY; i++) {
+      if (!bitSet.get(i)) {
+        return (bucket << 16) | i;
+      }
+    }
+    return -1;
   }
   @EpiTest(testDataFile = "absent_value_array.tsv")
   public static void findMissingElementWrapper(List<Integer> stream)
--- problem_mapping.js
+++ problem_mapping.js
@@ -1416,7 +1416,7 @@ problem_mapping = {
                 "total": 100
             },
             "Java: AbsentValueArray.java": {
-                "passed": 0,
+                "passed": 100,
                 "total": 100
             },
             "Python: absent_value_array.py": {
