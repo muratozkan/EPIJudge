169b29cb0166d58b2b60fbf1dd6d577006182d0a
Murat Özkan <smuratozkan@gmail.com>
2019-11-08 18:44:51 +0100

    11.8 [R] Kth Largest in Array

--- epi_judge_java/epi/KthLargestInArray.java
+++ epi_judge_java/epi/KthLargestInArray.java
@@ -2,9 +2,9 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Random;
 
 public class KthLargestInArray {
   // The numbering starts from one, i.e., if A = [3,1,-1,2] then
@@ -12,16 +12,34 @@ public class KthLargestInArray {
   // findKthLargest(3, A) returns 1, and findKthLargest(4, A) returns -1.
   @EpiTest(testDataFile = "kth_largest_in_array.tsv")
   public static int findKthLargest(int k, List<Integer> A) {
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, Comparator.naturalOrder());
-    // O(nlog(k)) since this method sorts the heap. Space complexity: O(k)
-    for (Integer val : A) {
-      int min = Integer.MIN_VALUE;
-      if (minHeap.size() == k) {
-        min = minHeap.remove();
+    Random random = new Random();
+    int p;  // current partition index
+    int si = 0, li = A.size() - 1;
+    do {
+      p = partition(A, si, li, si + random.nextInt(li - si + 1));
+      if (p > (k - 1)) {
+        li = p - 1;
+      } else if (p < (k - 1)) {
+        si = p + 1;
       }
-      minHeap.add(Math.max(min, val));
+    } while (p != (k - 1));
+
+    return A.get(p);
+  }
+
+
+  private static int partition(List<Integer> A, int start, int end, int pivotIndex) {
+    int pivotVal = A.get(pivotIndex);
+    int cutOff = start;
+    Collections.swap(A, pivotIndex, end); // put pivot to last slot so we can find it later
+    for (int i = start; i < end; i++) {
+      if (A.get(i) >= pivotVal) {
+        Collections.swap(A, i, cutOff);
+        cutOff += 1;
+      }
     }
-    return minHeap.remove();
+    Collections.swap(A, cutOff, end);
+    return cutOff;
   }
 
   public static void main(String[] args) {
