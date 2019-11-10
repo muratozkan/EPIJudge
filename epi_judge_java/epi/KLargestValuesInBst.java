20e8b04a581732735cc52b6ac7d95dac997b3b61
Murat Özkan <smuratozkan@gmail.com>
2019-11-10 16:57:28 +0100

    14.3 K Largest Values in BST

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -3,16 +3,27 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
 public class KLargestValuesInBst {
   @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
-
   public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+    List<Integer> result = new ArrayList<>(k);
+    reverseInOrder(tree, k, result);
+    return result;
   }
+
+  private static void reverseInOrder(BstNode<Integer> tree, int k, List<Integer> list) {
+    if (tree != null && list.size() < k) {
+      reverseInOrder(tree.right, k, list);
+      if (list.size() < k) {
+        list.add(tree.data);
+        reverseInOrder(tree.left, k, list);
+      }
+    }
+  }
+
   @EpiTestComparator
   public static boolean comp(List<Integer> expected, List<Integer> result) {
     if (result == null) {
