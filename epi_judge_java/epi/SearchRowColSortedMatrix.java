ae75a30f9af2f89b360ccfd3ff00162de1ebda62
Murat Özkan <smuratozkan@gmail.com>
2019-11-10 12:18:19 +0100

    11.6 Search Row and Col Sorted Array

--- epi_judge_java/epi/SearchRowColSortedMatrix.java
+++ epi_judge_java/epi/SearchRowColSortedMatrix.java
@@ -7,8 +7,20 @@ public class SearchRowColSortedMatrix {
   @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
 
   public static boolean matrixSearch(List<List<Integer>> A, int x) {
-    // TODO - you fill in here.
-    return true;
+    int m = 0, n = A.get(0).size() - 1;
+    while (m < A.size() && n >= 0) {
+      int t = A.get(m).get(n);
+      if (t == x) {
+        return true;
+      }
+      if (t > x) {
+        n -= 1;
+      } else {
+        m += 1;
+      }
+    }
+
+    return false;
   }
 
   public static void main(String[] args) {
