90c38776f376269c34889d07f54930840c2b980d
Murat Özkan <smuratozkan@gmail.com>
2019-11-20 21:52:53 +0100

    12.5 Find Neares Repeated Distance

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -2,13 +2,26 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
-
   public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
+    int minDist = Integer.MAX_VALUE;
+    Map<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < paragraph.size(); i++) {
+      String s = paragraph.get(i);
+      if (map.containsKey(s)) {
+        int pi = map.get(s);
+        if (minDist > i - pi) {
+          minDist = i - pi;
+        }
+      }
+      map.put(s, i);
+    }
+    return minDist == Integer.MAX_VALUE ? -1 : minDist;
   }
 
   public static void main(String[] args) {
