54aa0b2b0b27bef12773283a4a40cc8b7ddcbe7d
Murat Özkan <smuratozkan@gmail.com>
2019-11-20 21:05:46 +0100

    11.7 Find min and max simultaneously with minimum # of comparisons

--- epi_judge_java/epi/SearchForMinMaxInArray.java
+++ epi_judge_java/epi/SearchForMinMaxInArray.java
@@ -3,6 +3,7 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class SearchForMinMaxInArray {
   @EpiUserType(ctorParams = {Integer.class, Integer.class})
@@ -44,10 +45,31 @@ public class SearchForMinMaxInArray {
   }
 
   @EpiTest(testDataFile = "search_for_min_max_in_array.tsv")
-
   public static MinMax findMinMax(List<Integer> A) {
-    // TODO - you fill in here.
-    return new MinMax(0, 0);
+    if (A.size() == 1) {
+      return new MinMax(A.get(0), A.get(0));
+    }
+
+    for (int i = 0; i < A.size(); i += 2) {
+      int s = i + 1;
+      if (s == A.size()) {
+        s = i / 2;
+      }
+      int li = i;
+      if (A.get(i) > A.get(s)) {
+        li = s;
+      }
+      if (i == 0 && i != li) {  // first comparison and i > s
+        Collections.swap(A, i, s);
+      } else if (i > 0) {
+        Collections.swap(A, i / 2, li);
+      }
+
+    }
+    int min = A.stream().limit(A.size() / 2 + 1).reduce(Integer.MAX_VALUE, (a, b) -> b < a ? b : a);
+    int max = A.stream().skip(A.size() / 2).reduce(Integer.MIN_VALUE, (a, b) -> b > a ? b : a);
+
+    return new MinMax(min, max);
   }
 
   public static void main(String[] args) {
